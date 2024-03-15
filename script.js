f486e149b2623136dba7547ce36bde7da0671918
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-03-15 21:32:10 +0000

    Create html skeleton and write screen controller logic

--- script.js
+++ script.js
@@ -4,24 +4,39 @@ const gameboard = (function(){
     const board = [];
 
     for (let i=0; i<rows; i++){
-        board[i] = [];
-        for (j=0; j<columns; j++){
-            board[i].push('*');
-        }
+            board[i] = [];
+            for (j=0; j<columns; j++){
+                board[i].push(Cell());
+            }
     }
+
     const getBoard = () => board;
     const playerMove = (i, j, activePlayerToken) => {
-        console.log(activePlayerToken);
-        if (board[i][j] == '*') {
-            board[i][j] = activePlayerToken ;
-            console.log(board[i][j]);
+        if (board[i][j].getValue() == '*') {
+            board[i][j].addToken(activePlayerToken) ;
         }
     }
     const printBoard = () => {
         console.log (board);
     }
-    return {getBoard, playerMove, printBoard}
+    const resetBoard = () => {
+        for (let i=0; i<rows; i++){
+            for (j=0; j<columns; j++){
+                board[i][j].addToken('*');
+            }   
+        }    
+    }
+    return {getBoard, playerMove, printBoard, resetBoard}
 })();
+
+function Cell(){
+    let value = '*';
+    const addToken = (activePlayerToken) => {
+        value = activePlayerToken;
+    }
+    const getValue = () => value;
+    return {addToken, getValue};
+}
 
 const gameController = (function(playerOne = "Player 1", playerTwo = "Player 2") {
     const players = [
@@ -70,14 +85,14 @@ const gameController = (function(playerOne = "Player 1", playerTwo = "Player 2")
                 let win = true;
                 for (const pair of combo) {
                     let [row, col] = pair;
-                    if (gameboard.getBoard()[row][col] !== player.token) {
+                    if (gameboard.getBoard()[row][col].getValue() !== player.token) {
                         win = false;
                         break;
                     }
                 }
                 if (win) {
                     console.log (`${player.token} wins the game!`)
-                    //reset board
+                    gameboard.resetBoard();
                 }
             }
         }
@@ -86,6 +101,32 @@ const gameController = (function(playerOne = "Player 1", playerTwo = "Player 2")
     return {playRound, getActivePlayer}
 })();
 
-// const ScreenController = (function(){
-//     const game = gameController();
-// })();
+const ScreenController = (function(){
+    const turn = document.querySelector('.turn');
+    const board = document.querySelector('.board');
+
+    const updateScreen = () => {
+        board.textContent = "";
+        for (let i=0; i<3; i++){
+            for (j=0; j<3; j++){
+                const cellButton = document.createElement('button');
+                cellButton.classList.add('.cell');
+                cellButton.dataset.row = i;
+                cellButton.dataset.column = j;
+                cellButton.textContent = gameboard.getBoard()[i][j].getValue();
+                board.appendChild(cellButton);
+            }   
+        }    
+    }
+    function clickHandler(e) {
+        console.log ('test');
+        if (e.target.classList.contains('.cell')){
+            gameController.playRound(e.target.dataset.row, e.target.dataset.column);
+            updateScreen();
+        }
+
+    }
+    updateScreen();
+    board.addEventListener('click', clickHandler);
+    
+})();
